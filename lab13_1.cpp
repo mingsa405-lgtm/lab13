bba2caf970dbcc3553316362438a8b80ce9b7cb1
ming <mingsa405@gmail.com>
2026-01-06 21:28:14 +0700

    up

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,38 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double x[],int y,double z[]){
+    double mean;
+    double sum=0;
+    double sum2=0;
+    double sum3=1;
+    double sum4=0;
+    double Std;
+    double geo;
+    double har;
+    double  max=x[0];
+    double min=x[0];
+    for(int i=0;i<y;i++){
+          
+          sum=sum+x[i];
+          sum2=sum2+pow(x[i],2);
+          sum3=sum3*x[i];
+          sum4=sum4+(1/x[i]);
+          if(x[i]>max) max=x[i];
+          if(x[i]<min) min=x[i];
+    }
+    mean=sum/y;
+    Std = sqrt((sum2/y)-pow(mean,2));
+    geo=pow(sum3,1.0/y);
+    har=y/sum4;
+    z[0]=mean;
+    z[1]=Std;
+    z[2]=geo;
+    z[3]=har;
+    z[4]=max;
+    z[5]=min;
+    
+    
+    
+    
+}
